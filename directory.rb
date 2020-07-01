fe2b71826f3b6ae6bd9f7351eb2dba3091d0164f
Matteo Micozzi <micozzimatteo@yahoo.it>
2020-07-01 18:00:49 +0100

    Implemented list of students with hashes & symbols

--- directory.rb
+++ directory.rb
@@ -1,27 +1,27 @@
 # first we print the list of students
 students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex DeLarge",
-"The Wicked Witch of the West",
-"Terminator",
-"Freddy Krueger",
-"The Jocker",
-"Joffrey Baratheon",
-"Norman Bates"
+{name: "Dr. Hannibal Lecter", cohort: :november},
+{name: "Darth Vader", cohort: :november},
+{name: "Nurse Ratched", cohort: :november},
+{name: "Michael Corleone", cohort: :november},
+{name: "Alex DeLarge", cohort: :november},
+{name: "The Wicked Witch of the West", cohort: :november},
+{name: "Terminator", cohort: :november},
+{name: "Freddy Krueger", cohort: :november},
+{name: "The Jocker", cohort: :november},
+{name: "Joffrey Baratheon", cohort: :november},
+{name: "Norman Bates", cohort: :november}
 ]
 def print_header
 puts "The students of Villains Academy"
 puts "----------------------"
 end
-def print(names)
-names.each { |student| puts student }
+def print(student)
+student.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
 end
 # finally we print the total number of students
-def print_footer(names)
-puts "Overall, we have #{names.count} great students"
+def print_footer(stu_num)
+puts "Overall, we have #{stu_num.count} great students"
 end
 
 print_header
