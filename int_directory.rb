e55dcef33a24d9fd72157688b984425e6f8c20e3
Matteo Micozzi <micozzimatteo@yahoo.it>
2020-07-04 16:59:58 +0100

    Implemented file with saving data in file .csv

--- int_directory.rb
+++ int_directory.rb
@@ -28,6 +28,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to a students.csv"
   puts "9. Exit"
 end
 
@@ -37,12 +38,23 @@ def show_students
   print_footer
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each { |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line }
+  file.close
+end
+
 def process(selection)
   case selection
     when "1"
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
